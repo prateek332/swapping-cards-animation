db9c1ffac9c3927b8e4de917c912f8f0e5233c36
prateek-eyerov <prateek.p@eyerov.com>
2023-09-26 19:17:34 +0530

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import './styles.css';
+</script>
+
+<main>
+	<section class="card-container">
+		<div class="card front-card">Front Card</div>
+		<div class="card back-card">Back Card</div>
+	</section>
+</main>
